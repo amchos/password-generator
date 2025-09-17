f397bb79910c1f649a420dd3079e2d669c81cb1c
amchos <124582071+amchos@users.noreply.github.com>
2025-09-18 00:01:10 +0400

    password length aded
    toggles in process

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 // -add copy-on-click button
 // -toggle symbols and numbers to off/on
 
+const characterss = {
+    capChars: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    chars:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    specialSymbols: ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
+}
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
       "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=",
@@ -12,9 +18,17 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 
 const firstPassword = document.getElementById('first-password')
 const secondPassword = document.getElementById('second-password')
+const passwordLenElement = document.getElementById('password-length')
+
+let passwordLength = 15;
+
+passwordLenElement.addEventListener("change", function() {
+    passwordLength = passwordLenElement.value
+});
 
 firstPassword.addEventListener('click', async () => 
 	await navigator.clipboard.writeText(firstPassword.textContent)
+
 );
 secondPassword.addEventListener('click', async () => 
 	await navigator.clipboard.writeText(secondPassword.textContent)
@@ -23,7 +37,7 @@ secondPassword.addEventListener('click', async () =>
 function generatePassword() {
     let password = '';
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         password += characters[Math.floor( Math.random() * characters.length)]; 
     }
 
