088216c120fc716ede2696bce8307570fd114203
amchos <124582071+amchos@users.noreply.github.com>
2025-09-18 18:08:32 +0400

    toggles are working
    Still needs to be optimized. Now it builds a char array every time when the user generates a password, even if the toggles were not changed.

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// ---generate two random passwords
-// ---each should be 15 characters long
-// ---------bonus----------
-// -ability to set password length
-// -add copy-on-click button
-// -toggle symbols and numbers to off/on
-
-const characterss = {
-    capChars: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    chars:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    specialSymbols: ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
-}
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=",
-       "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
-
 const firstPassword = document.getElementById('first-password')
 const secondPassword = document.getElementById('second-password')
 const passwordLenElement = document.getElementById('password-length')
 
-let passwordLength = 15;
+const numbersAllowed = document.getElementById('numbers')
+const uppercaseAllowed = document.getElementById('uppercase')
+const lowercaseAllowed = document.getElementById('lowercase')
+const sybmolsAllowed = document.getElementById('symbols')
+
+
+let passwordLength = passwordLenElement.value;
 
 passwordLenElement.addEventListener("change", function() {
     passwordLength = passwordLenElement.value
@@ -34,11 +22,35 @@ secondPassword.addEventListener('click', async () =>
 	await navigator.clipboard.writeText(secondPassword.textContent)
 );
 
+function arrayMerge() {   //building 
+
+    let characters = []
+
+    if(numbersAllowed.checked)
+        characters.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    if (uppercaseAllowed.checked)
+        characters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    if(lowercaseAllowed.checked)
+        characters.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    if(sybmolsAllowed.checked)
+        characters.push("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/")
+
+    if(!numbersAllowed.checked && !uppercaseAllowed.checked && !lowercaseAllowed.checked && !sybmolsAllowed.checked) {
+        
+        uppercaseAllowed.checked = true
+        lowercaseAllowed.checked = true
+        numbersAllowed.checked = true
+        sybmolsAllowed.checked = true
+    }
+
+    return characters
+}
+
 function generatePassword() {
     let password = '';
-
+    let charsArr = arrayMerge()
     for (let i = 0; i < passwordLength; i++) {
-        password += characters[Math.floor( Math.random() * characters.length)]; 
+        password += charsArr[Math.floor( Math.random() * charsArr.length)]; 
     }
 
     return password
@@ -48,3 +60,5 @@ function generate() {
     firstPassword.textContent = generatePassword()
     secondPassword.textContent = generatePassword()
 }
+
+
